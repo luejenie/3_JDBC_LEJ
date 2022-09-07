0db4718e165a20a1f589bef5bf9c50318058d652
luejenie <luejenie@gmail.com>
2022-09-07 15:31:54 +0900

    20220907 JDBC_EmployeeManagement
    JDBC

--- EmployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
+++ EmployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
@@ -2,11 +2,14 @@ package edu.kh.emp.model.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.crypto.dsig.spec.ExcC14NParameterSpec;
 
 import edu.kh.emp.model.vo.Employee;
 
@@ -21,6 +24,17 @@ public class EmployeeDAO {
 	private ResultSet rs; 		 // -> 참조형의 기본값은 null;
 								 // -> 별도 초기화 안해도 된다!
 	
+	private PreparedStatement pstmt;
+	// Statement의 자식으로 향상된 기능을 제공
+	// -> ? 기호(placeholder, 위치홀더)를 이용해서
+	//	  SQL에 작성되어지는 리터럴을 동적으로 제어함.
+	
+	// SQL ?기호에 추가되는 값은
+	// 숫자인 경우 '' 없이 대입
+	// 문자열인 경우 ''가 자동으로 추가되어 대입.
+	
+	
+	
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	private String url = "jdbc:oracle:thin:@localhost:1521:XE";
 	private String user = "kh_lej";
@@ -31,7 +45,7 @@ public class EmployeeDAO {
 /* [차이점 구분]	
  * 	private Connection conn; // 필드(Heap, 변수가 비어있을 수 없음) 
  * 							    -> JVM이 지정한 기본값으로 초기화
- * 								-> 참조현의 기본값은 null;
+ * 								-> 참조형의 기본값은 null;
  *   							-> 별도 초기화 안해도 된다!  (=null 안해도 됨)
  * 
 	public void method() {
@@ -54,7 +68,7 @@ public class EmployeeDAO {
 			
 			//2. JDBC 참조 변수에 객체 대입
 			//-> conn, stmt, rs에 객체 대입
-			Class.forName(driver);  // 오라클 jdbc 드라이버 객체 메모리 로드
+			Class.forName(driver);  // 오라클 jdbc 드라이버 객체 메모리 로드  __드라이버를 객체로 만든다. Class.를 외치면 객체로 만들어주는..
 			conn = DriverManager.getConnection(url, user, pw);
 			// 오라클 jdbc 드라이버 객체를 이용하여 DB 접속 방법 생성
 			
@@ -93,11 +107,12 @@ public class EmployeeDAO {
 				int salary = rs.getInt("SALARY");
 				
 				
+				//__ 위에서 불러온 것을 Employee 객체에 담아주고
 				Employee emp = new Employee(empId, empName, empNo, email, phone, 
 										departmentTitle, jobName, salary);
 				empList.add(emp); // List 담기
 				
-			}  // while 종료   //__ 23퀴(전체개수) 돌고 empList에 담게 될 것
+			}  // while 종료   //__ 23번(전체개수) 돌고 empList에 담게 될 것
 			
 	
 		} catch(Exception e) {
@@ -122,5 +137,395 @@ public class EmployeeDAO {
 		return empList;
 	}
 
+
+
+	/** 사번이 일치하는 사원 정보 조회 DAO
+	 * @param empId   // 전달받은 값(전달인자): PARAMETER , 전달하는 값 : ARGUMENT
+	 * @return emp
+	 */
+	public Employee selectEmpId(int empId) {
+		// TODO Auto-generated method stub 자동완성으로 만듦. view에서.
+		
+		// 결과 저장용 변수를 선언
+		Employee emp = null;
+		// 만약 조회 결과가 있으면 Employee 객체를 생성해서 emp에 대입
+		// 만약 조회 결과가 없으면 emp에 아무것도 대입하지 않음(null)
+		// __ 위에서 이 자리에 list 선언했음
+		
+		try {   // __ 드라이버 불러오는 코드가 계속 반복될 것. 그렇지 않으면 아무것도 하지 않은 상태에서도 데이터베이스가 계속 불러와져서 문제 발생
+			
+			Class.forName(driver); // 오라클 JDBC 드라이버 메모리 로드
+			conn = DriverManager.getConnection(url, user, pw);  // 커넥션 생성해서 얻어오기
+			
+			// SQL 작성
+			String sql = "SELECT EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE,"
+					+ "	   NVL(DEPT_TITLE, '부서없음') DEPT_TITLE, JOB_NAME, SALARY"
+					+ " FROM EMPLOYEE"
+					+ " LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)"
+					+ " JOIN JOB USING (JOB_CODE)"
+					+ " WHERE EMP_ID = " + empId;
+											// View에서 입력받은 사번(파라미터)
+			
+			
+			// Statement 생성
+			stmt = conn.createStatement();
+					
+			// SQL 수행 후 결과 반환 받기
+			rs = stmt.executeQuery(sql);
+			
+			
+			// ** 조회 결과가 최대 1행인 경우  			 _== 우리가 결과가 하나라는 것을 확실하게 알고 있을 떄
+			//    불필요한 조건 검사를 줄이기 위해 if문 사용 권장 **     __ 원래 while문의 자리. while은 조건을 두 번 검사해서 불필요.
+			if(rs.next()) { // 조회 결과가 있을 경우
+				
+				// int empId = rs.getInt("EMP_ID");   // -> 파라미터(전달받은값)와 같은 값이므로 불필요 __이미 위에서 받았음.
+				String empName = rs.getString("EMP_NAME");
+				String empNo = rs.getString("EMP_NO");
+				String email = rs.getString("EMAIL");
+				String phone = rs.getString("PHONE");
+				String departmentTitle = rs.getString("DEPT_TITLE");
+				String jobName = rs.getString("JOB_NAME");
+				int salary = rs.getInt("SALARY");
+				
+				// 조회 결과를 담은 Employee 객체 생성 후
+				// 결과 저장용 변수 emp에 대입  ___위에서 선언
+				emp = new Employee(empId, empName, empNo, email, phone, departmentTitle, jobName, salary);
+					
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		
+		} finally {
+			
+			try {
+				
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(conn != null) conn.close();
+				
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			
+		}		
+		// 결과 반환
+		return emp;
+	}
+
+
+
+	/** 9. 주민등록번호가 일치하는 사원 정보 조회 DAO
+	 * @param empNo
+	 * @return emp
+	 */
+	public Employee selectEmpNO(String empNo) {
+		// TODO Auto-generated method stub
+		
+		// 결과 저장용 변수 선언
+		Employee emp = null;
+		
+		try {
+			// Connection 생성
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			
+			// SQP 작성
+			String sql = "SELECT EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE,"
+					+ "	   NVL(DEPT_TITLE, '부서없음') DEPT_TITLE, JOB_NAME, SALARY"
+					+ " FROM EMPLOYEE"
+					+ " LEFT JOIN DEPARTMENT ON (DEPT_ID = DEPT_CODE)"
+					+ " JOIN JOB USING (JOB_CODE)"
+					+ " WHERE EMP_NO = ?";
+									// placeholder
+			
+			// Statement 객체 사용시 순서
+			// SQL 작성 -> Statement 생성 -> SQL 수행 후 결과 반환(_이떄 매개변수로 SQL 사용)
+			
+			// PreparedStatement 객체 사용 시 순서
+			// SQL 작성 
+			// -> PreparedStatement 객체 생성(?가 포함된 SQL을 매개변수로 사용)  __기존과 다르게 SQL을 먼저 사용
+			// -> ?에 알맞은 값 대입
+			// -> SQL 수행 후 결과 반환
+			
+			
+			
+			
+			// PreparedStatement 객체 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			// ?에 알맞은 값 대입
+			pstmt.setString(1, empNo);
+			
+			// SQL 수행 후 결과 반환
+			rs = pstmt.executeQuery();
+			// PreparedStatement는
+			// 객체 생성 시 이미 SQL이 담겨져 있는 상태이므로
+			// SQL 수행 시 매개변수로 전달할 필요가 없다!   __cf. rs = stmt.executeQuery(sql);
+			// -> 실수로 SQL을 매개변수에 추가하면
+			//   ?에 작성(추가)했던 값이 모두 사라져 수행 시 오류 발생
+			
+			
+			
+			if(rs.next()) {
+				
+				int empId = rs.getInt("EMP_ID"); 
+				String empName = rs.getString("EMP_NAME");
+//				String empNo = rs.getString("EMP_NO");  // -> 파라미터와 같은 값이므로 불필요
+				String email = rs.getString("EMAIL");
+				String phone = rs.getString("PHONE");
+				String departmentTitle = rs.getString("DEPT_TITLE");
+				String jobName = rs.getString("JOB_NAME");
+				int salary = rs.getInt("SALARY");
+				
+				emp = new Employee(empId, empName, empNo, email, phone, departmentTitle, jobName, salary);
+					
+				
+			}
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+	
+		} finally {
+			
+			try {
+			
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(conn != null) conn.close();
+				
+					
+			} catch(Exception e) {
+				e.printStackTrace();
+				
+			}
+			
+		}
+		// 결과 반환
+		return emp;
+		
+	}
+
+
+	
+	
+	
+
+	/** 1. 새로운 사원 정보 추가 DAO
+	 * @param emp
+	 * @return result(INSERT 성공한 행의 개수 반환)
+	 */
+	public int insertEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		// 결과 저장용 변수 선언
+		int result = 0;
+		
+		try {
+			
+			//커넥션 생성
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			
+			// ** DML 수행할 예정 **
+			// - 트랜잭션에 DML 구문이 임시 저장
+			// -> 정상적인 DML인지를 판별해서 개발자가 직접 COMMIT, ROLLBACK을 수행
+			
+			// 하지만.. Connection 객체 생성 시
+			// AutoCommit이 활성화되어 있는 상태이기 때문에
+			// 이를 해제하는 코드를 추가!!!!!!!!!!!!!!!!!!★★★★★
+			conn.setAutoCommit(false);  // AutoCommit 비활성화
+			// -> AutoCommit 비활성화해도
+			//    conn.close() 구문이 수행되면 자동으로 Commit이 수행됨.
+			// --> close() 수행 전에 트랜잭션 제어 코드를 작성해야 함!
+			
+			
+			// SQL 작성
+			String sql 
+			= "INSERT INTO EMPLOYEE VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, SYSDATE, NULL, DEFAULT)";
+			// 퇴사여부 컬럼의 DEFAULT == 'N'                                   //입사일,  퇴사일, 퇴사(디폴트값 = 'N')
+			
+			
+			// PreaparedStatement 객체 생성(매개변수에 SQL 추가)
+			pstmt = conn.prepareStatement(sql);
+			
+			// ?(placeholder)에 알맞은 값 대입
+			
+			 pstmt.setInt(1, emp.getEmpId());   //_메서드 매개변수 emp -> emp.get~~
+	         pstmt.setString(2, emp.getEmpName());
+	         pstmt.setString(3, emp.getEmpNo());
+	         pstmt.setString(4, emp.getEmail());
+	         pstmt.setString(5, emp.getPhone());
+	         pstmt.setString(6, emp.getDeptCode());
+	         pstmt.setString(7, emp.getJobCode());
+	         pstmt.setString(8, emp.getSalLevel());
+	         pstmt.setInt(9, emp.getSalary());
+	         pstmt.setDouble(10, emp.getBonus());
+	         pstmt.setInt(11, emp.getManagerId());
+			
+			// SQL 수행 후 결과 반환
+			result = pstmt.executeUpdate();
+			// executeUpdate(): DML(INSERT, UPDATE, DELETE) 수행 후 결과 행 '개수' 반환
+			// executeQuery(): SELECET 실행 후 ResultSet 반환
+			
+			
+			// *** 트랜잭션 제어 처리 ***
+			// -> DML 성공 여부에 따라서 COMMIT, ROLLBACK 제어
+			if(result > 0) conn.commit();    // DML 성공 시 commit
+			else		   conn.rollback();  // DML 실패 시 rollback
+			
+			
+			
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();			
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}	
+		}
+		
+		// 결과 반환
+		return result;
+	}
+
+
+
+	/** 4. 사원이 일치하는 사원 정보 수정(이메일, 전화번호, 급여)
+	 * @param emp
+	 * @return result
+	 */
+	public int updateEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		int result = 0; // 결과 저장용 변수
+		
+		
+		try {
+			// 커넥션 생성
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			conn.setAutoCommit(false);  // AutoCommint 비활성화
+			
+			String sql = "UPDATE EMPLOYEE SET"
+					+ " EMAIL = ?, PHONE = ?, SALARY = ?"
+					+ " WHERE EMP_ID = ?";
+			
+			// PreparedStatement 생성
+			pstmt = conn.prepareStatement(sql);
+					
+			// ?에 알맞은 값을 세팅
+			pstmt.setString(1, emp.getEmail());
+			pstmt.setString(2, emp.getPhone());
+			pstmt.setInt(3, emp.getSalary());
+			pstmt.setInt(4, emp.getEmpId());
+			
+			result = pstmt.executeUpdate();  // 반영된 행의 개수 반환
+			
+			// 트래잭션 제어 처리
+			if(result == 0)  conn.rollback();
+			else	         conn.commit();
+			
+						
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				//JDBC 객체 자원 반환
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result; // 결과 반환
+	}
+
+
+
+	
+	
+	
+	
+	
+	/** 5. 사번이 일치하는 사원 정보 삭제
+	 * @param emp
+	 * @return result
+	 */
+	public int deleteEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		int result = 0;
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			conn.setAutoCommit(false);
+			
+			String sql = "DELETE FROM EMPLOYEE WHERE EMP_ID = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, emp.getEmpId());
+			
+			result = pstmt.executeUpdate();
+			
+			if(result > 0)  conn.commit();
+			else		    conn.rollback();
+		
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			try { 
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			
+		}
+		
+		return result;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- EmployeeManagement/src/edu/kh/emp/view/EmployeeView.java
+++ EmployeeManagement/src/edu/kh/emp/view/EmployeeView.java
@@ -28,7 +28,7 @@ public class EmployeeView {
 			try {
 				
 				System.out.println("---------------------------------------------------------");
-				System.out.println("----- 사원 관리 프로그램 -----");
+				System.out.println("[사원 관리 프로그램]");
 				System.out.println("1. 새로운 사원 정보 추가");
 				System.out.println("2. 전체 사원 정보 조회");
 				System.out.println("3. 사번이 일치하는 사원 정보 조회");
@@ -37,6 +37,9 @@ public class EmployeeView {
 				System.out.println("6. 입력 받은 부서와 일치 모든 사원 정보 조회");
 				System.out.println("7. 입력 받은 급여 이상을 받는 모든 사원 정보 조회");
 				System.out.println("8. 부서별 급여 합 전체 조회");
+				
+				System.out.println("9. 주민등록번호가 일치하는 사원 정보 조회");
+				
 				System.out.println("0. 프로그램 종료");
 				
 				System.out.print("메뉴 선택 >> ");
@@ -46,14 +49,15 @@ public class EmployeeView {
 				
 				
 				switch(input) {
-				case 1 :  break;
+				case 1 : insertEmployee(); break;
 				case 2 : selectAll(); break;
-				case 3 :  break;
-				case 4 :  break;
-				case 5 :  break;
+				case 3 : selectEmpId(); break;
+				case 4 : updateEmployee(); break;	//이메일, 전화번호, 급여만 바꾸기
+				case 5 : deleteEmployee(); break;   // 사번일치가 3,4,5번에서 사용되므로, 사번 일치하는 코드만 따로 생성하기
 				case 6 :  break;
 				case 7 :  break;
 				case 8 :  break;
+				case 9 : selectEmpNo(); break;
 				case 0 : System.out.println("프로그램을 종료합니다."); break;
 				default : System.out.println("메뉴에 존재하는 번호만 입력하세요.");
 				}
@@ -77,7 +81,7 @@ public class EmployeeView {
 	
 	
 	/** 
-	 * 전체 사원 정보 조회
+	 * 2. 전체 사원 정보 조회
 	 */
 	public void selectAll() {
 		System.out.println("<전체 사원 정보 조회>");
@@ -101,7 +105,7 @@ public class EmployeeView {
 			System.out.println("조회된 사원 정보가 없습니다.");
 			
 		} else {
-			System.out.println("사번 |   이름  | 주민 등록 번호 |        이메일        |   전화 번호   | 부서 | 직책 | 급여" );
+			System.out.println(" 사번  |  이름   |   주민 등록 번호   |         이메일         |   전화 번호   |  부서  | 직책 | 급여 ");
 			System.out.println("------------------------------------------------------------------------------------------------");
 			for(Employee emp : empList) { 
 				System.out.printf(" %2d  | %4s | %s | %20s | %s | %s | %s | %d\n",
@@ -115,7 +119,238 @@ public class EmployeeView {
 	
 	
 	
+	/**
+	 * 3. 사번이 일치하는 사원 정보 조회    // 사번이 일치하는 사람은 1명뿐이다.
+	 */
+	public void selectEmpId() {
+		System.out.println("<사번이 일치하는 사원 정보 조회>");
+		
+		// 사번 입력 받기
+		int empId = inputEmpId();
+		
+		// 입력 받은 사번을 DAO의 selectEmpId() 메서드로 전달하여 
+		// 조회된 사원 정보 반환 받기
+		Employee emp = dao.selectEmpId(empId);   
+		// _ 데이터베이스에서 딱 한명의 정보를 가지고 오는데, List는 여러명의 정보를 사용하기 때문에 적절하지 않음.
+		// _ 그래서 vo 사용 -> VO(Value Object) : 값 저장용 객체 (== DB 조회 결과 "한 행"을 저장)
+		
+		printOne(emp);  //조회 결과 출력
+		
+		
+	}
 	
+	
+	/** 사번을 입력 받아 반환하는 메서드
+	 * @return
+	 */
+	public int inputEmpId() {
+		System.out.print("사번 입력 : ");
+		int empId = sc.nextInt();
+		sc.nextLine();  // 버어페 남은 개행문자 제거   __추후에 혹시 모를 경우를 위해		
+		return empId;		
+	}
+	
+	
+	
+	/** 사원 1명 정보 출력
+	 * @param emp
+	 */
+	public void printOne(Employee emp) {
+		
+		if(emp == null) {
+			System.out.println("조회된 사원 정보가 없습니다.");
+			
+		} else {
+			System.out.println(" 사번  |  이름   |   주민 등록 번호   |         이메일         |   전화 번호   |  부서  | 직책 | 급여 " );
+			System.out.println("------------------------------------------------------------------------------------------------");
+			
+				System.out.printf(" %2d  | %4s | %s | %20s | %s | %s | %s | %d\n",
+						emp.getEmpId(), emp.getEmpName(), emp.getEmpNo(), emp.getEmail(), 
+						emp.getPhone(), emp.getDepartmentTitle(), emp.getJobName(), emp.getSalary());
+		}
+		
+	}
+	
+	
+	
+	/**
+	 * 9. 주민등록번호가 일치하는 사원 정보 조회
+	 */
+	public void selectEmpNo() {
+		System.out.println("<주민등록번호가 일치하는 사원 정보 조회>");
+		
+		System.out.print("주민등록번호 입력 : ");   //_사번과 다른 점은 숫자가 아닌 문자열임
+		String empNo = sc.next();
+		
+		Employee emp = dao.selectEmpNO(empNo);   //_사원 한 명의 정보임
+		
+		printOne(emp);
+		
+	}
+	
+	
+	
+	/**
+	 * 1. 새로운 사원 정보 추가
+	 */
+	public void insertEmployee() {
+		System.out.println("<사원 정보 추가>");   //11개 정보
+		
+		
+		int empId = inputEmpId();
+				
+		System.out.print("이름 : ");
+		String empName = sc.next();
+		
+		System.out.print("주민등록번호 : ");
+		String empNo = sc.next();
+		
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		
+		System.out.print("전화번호(-제외) : ");
+		String phone = sc.next();
+		
+		System.out.print("부서코드(D1~D9) : ");
+		String deptCode = sc.next();
+		
+		System.out.print("직급코드(J1~J7) : ");
+		String jobCode = sc.next();
+		
+		System.out.print("급여등급(S1~S6) : ");
+		String salLevel = sc.next();
+		
+		System.out.print("급여 : ");
+		int salary = sc.nextInt();
+		
+		System.out.print("보너스 : ");
+		double bonus = sc.nextDouble();
+		
+		System.out.print("사수번호 : ");
+		int managerId = sc.nextInt();
+		
+		
+		// 입력 받은 값을 Employee 객체에 담아서 DAO로 전달
+		Employee emp = new Employee(empId, empName, empNo, email, phone, salary, deptCode, jobCode, salLevel, bonus, managerId);
+		int result = dao.insertEmployee(emp);   //__ select였다면 list를 사용했을텐데, 이건 insert -> 
+		// INSERT, UPDATE, DELETE 같은 DML 구문은
+		// 수행 후 테이블에 반영된 행의 개수를 반환함.   __dbeaver에서 DML 하면 n행이 성공했습니다~ 이런식으로 나오는 걸 기억!
+		// -> 조건이 잘못된 경우 반영된 행이 없으므로 0 반환
+		
+		if(result > 0) { // DML 구문 성공시
+			System.out.println("사원 정보 추가 성공");
+			
+		} else { // DML 구문 실패 시
+			System.out.println("사원 정보 추가 실패...");
+				
+		}
+	}
+	
+	
+	
+	/**
+	 * 4. 사원이 일치하는 사원 정보 수정(이메일, 전화번호, 급여)
+	 */
+	public void updateEmployee() {
+		System.out.println("<사번이 일치하는 사원 정보 수정>");
+		
+		int empId = inputEmpId();
+		
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		
+		System.out.print("전화번호(- 제외) : ");
+		String phone = sc.next();
+		
+		System.out.print("급여 : ");
+		int salary = sc.nextInt();
+		
+		
+		// 기본생성자로 객체 생성 후 setter를 이용해서 초기화
+		Employee emp = new Employee();  // _ 기본생성자 생성, 초기화 안 된 상태!
+		emp.setEmpId(empId);
+		emp.setEmail(email);
+		emp.setPhone(phone);
+		emp.setSalary(salary);
+		
+		int result = dao.updateEmployee(emp);  // UPDATE(DML) -> 반영된 행의 개수 반환(int형)
+		
+		if(result > 0) {
+			System.out.println("사원 정보가 수정되었습니다.");
+		} else {
+			System.out.println("사번이 일치하는 사원이 존재하지 않습니다.");
+		}
+				
+	}
+	
+	
+	/**
+	 * 5. 사번이 일치하는 사원 정보 삭제
+	 */
+	public void deleteEmployee() {
+		System.out.println("<사번이 일치하는 사원 정보 삭제>");
+		
+		int empId = inputEmpId();  // 사번 입력
+			
+		// 사번 입력하면 사번이랑 이름 출력되도록.
+		Employee emp1 = dao.selectEmpId(empId); 
+//		print2(emp1);
+		if(emp1 == null) {
+			System.out.println("조회된 사원 정보가 없어 삭제할 수 없습니다.");
+						
+			
+		} else {
+			System.out.printf("[사번 :   %2d]  \n[이름 : %4s] \n", emp1.getEmpId(), emp1.getEmpName());
+						
+			System.out.print("\n정말 삭제하시겠습니까?(Y/N) ");
+			char input = sc.next().toUpperCase().charAt(0); // Y/N 대소문자 구분 없이 입력
+			// -> 모두 대문자로 변환됨
+			
+			if(input == 'Y') {
+				// 삭제를 수행하는 DAO 호출
+				// 성공 : "삭제되었습니다."
+				// 실패 : "사번이 일치하는 사원이 존재하지 않습니다." - 출력
+				
+				Employee emp = new Employee();
+				emp.setEmpId(empId);
+				
+				int result = dao.deleteEmployee(emp);
+				
+				if(result > 0) {
+					System.out.println("삭제되었습니다.");
+				} else {
+					System.out.println("사번이 일치하는 사원이 존재하지 않습니다.");
+				}
+				
+				
+			} else {
+				System.out.println("취소되었습니다.");
+			}
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	/** 사원 1명 정보 출력(사번, 이름)   // 사원 정보 삭제 시, 사번 입력하면 관련 정보 뜨게하고 싶어서 만들어 본 것 (수업x)
+	 * @param emp
+	 */
+	public void print2(Employee emp) {
+		
+		if(emp == null) {
+			System.out.println("조회된 사원 정보가 없습니다.");
+			
+			
+		} else {
+			System.out.printf("사번 : %2d  \n이름 : %4s \n", emp.getEmpId(), emp.getEmpName());
+						
+		}
+		
+	}
 	
 	
 	
