05f8b076d3a2e38d27885a13a7fd6975e837d7d9
luejenie <luejenie@gmail.com>
2022-09-19 17:51:12 +0900

    개인 복습(JDBC_member_board)

---
+++ review_member_board/src/re/lej/jdbc/model/service/Service.java
@@ -0,0 +1,33 @@
+package re.lej.jdbc.model.service;
+
+import static re.lej.jdbc.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import re.lej.jdbc.model.dao.DAO;
+
+public class Service {
+	
+	private DAO dao = new DAO();
+
+	/** 아이디 중복 검사 서비스
+	 * @param memberId
+	 * @return result
+	 */
+	public int idDupCheck(String memberId) {
+		Connection conn = getConnection();
+		
+		int result = dao.idDupCheck(conn, memberId);
+		close(conn);
+				
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ review_member_board/src/re/lej/jdbc/run/Run.java
@@ -0,0 +1,12 @@
+package re.lej.jdbc.run;
+
+import re.lej.jdbc.view.View;
+
+public class Run {
+	public static void main(String[] args) {
+		
+		new View().mainMenu();
+
+	}
+
+}
